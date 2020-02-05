a1ca7b562ea9e458240f67bac31b1afba79e29d8
Martha Hipley <info@marthahipley.com>
2020-02-05 13:21:47 -0600

    completing section 4

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,34 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+	////My solution:
+	// let reversed = "";
+	// for (let char of str) {
+	// 	reversed = char + reversed;
+	// }
+	// if (reversed == str) {
+	// 	return true;
+	// }
+	// else {
+	// 	return false;
+	// }
+
+	////Course solution 1 - most straightforward:
+	//const reversed = str.split('').reverse().join('');
+
+	////(Boolean statement - returns true if the two constants are equal & false if not.)
+	//return str === reversed;
+
+	////Course solution 2 - using .every() helper, but not ideal (redundant)
+	////(useful as a way to show that you can tell when a solution is suboptimal)
+	return str.split('').every((char, i) => {
+		return char === str[str.length - i - 1];
+
+	});
+
+
+}
 
 module.exports = palindrome;
